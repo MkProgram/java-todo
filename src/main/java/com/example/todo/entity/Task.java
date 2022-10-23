b2d549cf00b243e5429a6800182b60e2e613d8b0
MkProgram <marian.kassel@t-online.de>
2022-10-23 15:45:58 +0200

    Initial Commit

---
+++ src/main/java/com/example/todo/entity/Task.java
@@ -0,0 +1,28 @@
+package com.example.todo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private boolean completed;
+
+    @Column
+    private int priority;
+}
